a5e4eb3fedc54e83d670bdb6de1cb694c8dbd64a
devnacer <kalache.nacer.kn@gmail.com>
2024-06-23 20:43:45 +0100

    first commit

---
+++ tt.php
@@ -0,0 +1,7 @@
+$phpmailer = new PHPMailer();
+$phpmailer->isSMTP();
+$phpmailer->Host = 'sandbox.smtp.mailtrap.io';
+$phpmailer->SMTPAuth = true;
+$phpmailer->Port = 2525;
+$phpmailer->Username = 'bc81e0e1eda2ab';
+$phpmailer->Password = '55fba800ca0361';
